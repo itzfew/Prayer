dbf134255a7721974d5ea22e269c4f28183ebca9
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-04-01 21:23:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ navigator.geolocation.getCurrentPosition(function(position) {
     var date = new Date();
     var prayerTimes = prayTimes.getTimes(date, coordinates, 'auto', 'auto', '24h');
 
+    // Get the location name using reverse geocoding (optional)
+    fetchLocationName(coordinates[0], coordinates[1]);
+
     // Display prayer times in HTML
+    var locationDiv = document.getElementById('location');
+    locationDiv.innerHTML = 'Location: Loading...';
+
     var prayerTimesDiv = document.getElementById('prayerTimes');
     prayerTimesDiv.innerHTML = `
         <p>Imsak: ${prayerTimes.imsak}</p>
@@ -20,3 +26,15 @@ navigator.geolocation.getCurrentPosition(function(position) {
 }, function(error) {
     console.error('Error getting user location:', error);
 });
+
+// Function to fetch location name using reverse geocoding
+function fetchLocationName(latitude, longitude) {
+    var url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var locationDiv = document.getElementById('location');
+            locationDiv.innerHTML = `Location: ${data.city}, ${data.countryName}`;
+        })
+        .catch(error => console.error('Error fetching location:', error));
+}
