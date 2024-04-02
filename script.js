ea83b3e5d427475d16bc556ef1ce7f5c290deef7
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-04-02 11:13:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,49 @@
-// Fetch prayer times using the PrayTimes.js library
-navigator.geolocation.getCurrentPosition(function(position) {
-    var coordinates = [position.coords.latitude, position.coords.longitude];
-    var prayerTimesDiv = document.getElementById('prayerTimes');
-    var locationDiv = document.getElementById('location');
-
-    // Update date and address periodically
-    updateDate();
-    fetchAddress(coordinates);
-
-    // Get and display prayer times
-    updatePrayerTimes();
-
-    // Function to update date periodically
-    function updateDate() {
-        setInterval(function() {
-            var currentDate = new Date();
-            var hijriDate = getHijriDate(currentDate);
-            var gregorianDate = currentDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-            document.querySelector('.hijri').innerText = 'Hijri Date: ' + hijriDate;
-            document.querySelector('.gregorian').innerText = 'Gregorian Date: ' + gregorianDate;
-        }, 60000); // Update every minute
-    }
-
-    // Function to update prayer times
-    function updatePrayerTimes() {
-        var date = new Date();
-        var prayerTimes = prayTimes.getTimes(date, coordinates, 'auto', 'auto', '24h');
-
-        // Display prayer times in HTML
-        var prayerTimesHTML = '';
-        for (var prayer in prayerTimes) {
-            if (prayerTimes.hasOwnProperty(prayer)) {
-                prayerTimesHTML += `
-                    <div class="row">
-                        <div class="column">
-                            <p class="prayer-name">${prayer}</p>
-                            <p class="prayer-time">${prayerTimes[prayer]}</p>
-                        </div>
-                    </div>
-                `;
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to get user's current location
+    function getUserLocation() {
+        return new Promise((resolve, reject) => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    position => {
+                        resolve(position.coords);
+                    },
+                    error => {
+                        reject(error);
+                    }
+                );
+            } else {
+                reject("Geolocation is not supported by this browser.");
             }
-        }
-        prayerTimesDiv.innerHTML = prayerTimesHTML;
+        });
     }
 
-    // Function to fetch present address using reverse geocoding
-    function fetchAddress(coordinates) {
-        var url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coordinates[0]}&longitude=${coordinates[1]}&localityLanguage=en`;
-        fetch(url)
+    // Function to fetch prayer times using user's location
+    function fetchPrayerTimes(latitude, longitude) {
+        const url = `http://api.aladhan.com/v1/timingsByCoordinates/${Date.now()}?latitude=${latitude}&longitude=${longitude}&method=2`;
+        return fetch(url)
             .then(response => response.json())
-            .then(data => {
-                locationDiv.innerHTML = `Location: ${data.city}, ${data.localityInfo.administrative[1].name}, ${data.countryName}`;
-            })
-            .catch(error => console.error('Error fetching location:', error));
+            .then(data => data.data.timings)
+            .catch(error => {
+                console.error('Error fetching prayer times:', error);
+                throw error;
+            });
     }
-});
 
-// Function to get Hijri date
-function getHijriDate(date) {
-    var hijriDate = new Intl.DateTimeFormat('ar-u-ca-islamic', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    }).format(date);
-    return hijriDate;
-}
+    // Function to display prayer times
+    function displayPrayerTimes(prayerTimes) {
+        const prayerList = document.getElementById('prayer-list');
+        prayerList.innerHTML = ''; // Clear previous list items
+
+        for (const [prayer, time] of Object.entries(prayerTimes)) {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `<strong>${prayer}:</strong> ${time}`;
+            prayerList.appendChild(listItem);
+        }
+    }
+
+    // Get user's location and fetch prayer times
+    getUserLocation()
+        .then(coords => fetchPrayerTimes(coords.latitude, coords.longitude))
+        .then(prayerTimes => displayPrayerTimes(prayerTimes))
+        .catch(error => console.error('Error getting user location:', error));
+});
